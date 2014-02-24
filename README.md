c1651cd9e7f00dfe3c0d6fd5fa6a8c8cd9adfc83
Steve Pousty <scitronpousty@gmail.com>
2014-02-23 19:26:46 -0800

    better code example

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
-The OpenShift `jbosseap` cartridge documentation can be found at:
+javaparks
+=========
 
-https://github.com/openshift/origin-server/tree/master/cartridges/openshift-origin-cartridge-jbosseap/README.md
+National Parks front end and backend service for use in Mongo. This is the Java version of the web service
+
+The front page with the leaflet example is in src/main/webapp/index.html
+
+Here is the page with the signatures for the web services and what the MongoDB queries look like
+
+https://github.com/thesteve0/javaparks/blob/master/src/main/java/org/openshift/webservice/ParkWS.java
+
---
+++ src/main/java/org/openshift/webservice/ParkWS.java
@@ -0,0 +1,239 @@
+package org.openshift.webservice;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+
+import org.bson.types.ObjectId;
+import org.openshift.data.DBConnection;
+import org.openshift.data.Park;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import com.mongodb.WriteConcern;
+
+
+@RequestScoped
+@Path("/parks")
+public class ParkWS {
+	
+	@Inject 
+	private DBConnection dbConnection;
+	
+	private HashMap processPark(DBObject incoming){
+		HashMap holder = new HashMap<String, Object>();
+		holder.put("name",dataValue.get("Name"));
+		holder.put("position", dataValue.get("pos"));
+		holder.put("id", dataValue.get("_id").toString());
+
+		
+	}
+	
+	//get all the parks
+	@GET()
+	@Produces("application/json")
+	public List getAllParks(){
+		ArrayList allParksList = new ArrayList();
+		DB db = dbConnection.getDB();
+		DBCollection parkListCollection = db.getCollection("parkpoints");
+		DBCursor cursor = parkListCollection.find();
+		try {
+			while(cursor.hasNext()) {				
+				allParksList.add(this.processPark(cursor.next()));				
+            }
+        } finally {
+            cursor.close();
+        }
+
+		return allParksList;
+	}
+	
+	
+	
+	//////////get a park given the id
+	@GET()
+	@Produces("application/json")
+	@Path("park/{id}")
+	public HashMap getAPark(@PathParam("id") String id){
+		
+		DB db = dbConnection.getDB();
+		DBCollection parkListCollection = db.getCollection("parkpoints");
+		
+		DBObject park = parkListCollection.findOne(new BasicDBObject().append("_id",  new ObjectId(id)));
+		HashMap holder = new HashMap<String, Object>();
+		holder.put("name",park.get("Name"));
+		holder.put("position", park.get("pos"));
+		holder.put("id", park.get("_id").toString());
+		return holder;
+	}
+	
+	///////////get parks within a bounding box (used for map application)
+	@GET
+	@Produces("application/json")
+	@Path("within")
+	public List findParksWithin(@QueryParam("lat1") float lat1, @QueryParam("lon1") float lon1, @QueryParam("lat2") float lat2, @QueryParam("lon2") float lon2){
+		ArrayList<Map> allParksList = new ArrayList<Map>();
+		DB db = dbConnection.getDB();
+		DBCollection parkListCollection = db.getCollection("parkpoints");
+		
+		//make the query object
+		BasicDBObject spatialQuery = new BasicDBObject();
+		ArrayList posList = new ArrayList();
+		ArrayList firstPair = new ArrayList();
+		ArrayList secondPair = new ArrayList();
+		firstPair.add(new Float(lon1));
+		firstPair.add(new Float(lat1));
+		secondPair.add(new Float(lon2));
+		secondPair.add(new Float(lat2));
+		
+		posList.add(firstPair);
+		posList.add(secondPair);
+		
+		BasicDBObject boxQuery = new BasicDBObject();
+		boxQuery.put("$box", posList);
+		
+		spatialQuery.put("pos", new BasicDBObject("$within", boxQuery));
+		
+		System.out.println("within spatial query: " + spatialQuery.toString());
+		
+		
+		DBCursor cursor = parkListCollection.find(spatialQuery);
+		try {
+			while(cursor.hasNext()) {				
+				allParksList.add(this.processPark(cursor.next()));				
+            }
+        } finally {
+            cursor.close();
+        }
+
+
+		
+		
+		return allParksList;
+		
+	}
+	
+		
+	///////////get parks near a coord  ?lat=37.5&lon=-83.0
+	@GET
+	@Produces("application/json")
+	@Path("near")
+	public List findParksNear(@QueryParam("lat") float lat, @QueryParam("lon") float lon){
+		ArrayList<Map> allParksList = new ArrayList<Map>();
+		DB db = dbConnection.getDB();
+		DBCollection parkListCollection = db.getCollection("parkpoints");
+		
+		//make the query object
+		BasicDBObject spatialQuery = new BasicDBObject();
+		ArrayList posList = new ArrayList();
+		posList.add(new Float(lon));
+		posList.add(new Float(lat));
+		spatialQuery.put("pos", new BasicDBObject("$near", posList));
+		
+		System.out.println("spatial query: " + spatialQuery.toString());
+		
+		
+		DBCursor cursor = parkListCollection.find(spatialQuery);
+		try {
+			while(cursor.hasNext()) {				
+				allParksList.add(this.processPark(cursor.next()));				
+            }
+        } finally {
+            cursor.close();
+        }
+
+		return allParksList;
+		
+	}
+	
+	
+	////////get parks near a coord with a certain name
+	@GET
+	@Produces("application/json")
+	@Path("name/near/{name}")
+	public List findParksNearName(@PathParam("name") String name, @QueryParam("lat") float lat, @QueryParam("lon") float lon){
+		ArrayList<Map> allParksList = new ArrayList<Map>();
+		DB db = dbConnection.getDB();
+		DBCollection parkListCollection = db.getCollection("parkpoints");
+		
+		//make the query object
+		BasicDBObject wholeQuery = new BasicDBObject();
+		//first the regex
+		Pattern namePattern = Pattern.compile(name, Pattern.CASE_INSENSITIVE);
+		wholeQuery.put("Name", namePattern);
+		
+		/*then the spatial */
+		ArrayList posList = new ArrayList();
+		posList.add(new Float(lon));
+		posList.add(new Float(lat));
+		wholeQuery.put("pos", new BasicDBObject("$near", posList));
+		
+		
+		System.out.println("whole query: " + wholeQuery.toString());
+		
+		
+		DBCursor cursor = parkListCollection.find(wholeQuery);
+		try {
+			while(cursor.hasNext()) {				
+				allParksList.add(this.processPark(cursor.next()));				
+            }
+        } finally {
+            cursor.close();
+        }
+
+		return allParksList;
+		
+	}
+	
+	
+	///////still need the insert
+	@POST
+	@Path("park")
+	@Consumes("application/json")
+	@Produces("application/json")
+	public HashMap insertAPark(Park park){
+		BasicDBObject parkObject = new BasicDBObject("Name",park.getName());
+		parkObject.append("pos", park.getPos());
+		
+		DB db = dbConnection.getDB();
+		DBCollection parkListCollection = db.getCollection("parkpoints");
+		try{
+			parkListCollection.insert(parkObject, WriteConcern.SAFE);
+		} catch (Exception e) {
+			System.out.println("threw an exception: " + e.getClass() + " :: " + e.getMessage());
+		}
+		
+		//now make it look pretty
+		HashMap holder = new HashMap<String, Object>();
+		holder.put("name",parkObject.get("Name"));
+		holder.put("position", parkObject.get("pos"));
+		holder.put("id", parkObject.get("_id").toString());
+		return holder;
+		
+	}
+	
+	
+/****** Just for testing purposes ***********/	
+	@GET()
+	@Path("/test")
+	@Produces("text/plain")
+	public String sayHello() {
+	    return "Hello World from JAX-RS";
+	}
+
+}
